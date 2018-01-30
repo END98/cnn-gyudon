dc2d8ed000b237af40fd52c20e02f34d63de1c37
END98 <takayoshi-endo@spicebox.co.jp>
2018-01-30 18:30:38 +0900

    ADD: image

--- tornado/app.py
+++ tornado/app.py
@@ -1,5 +1,6 @@
 # https://qiita.com/Hironsan/items/f4100e2884efd2ad215d#_reference-94b6d787ea68ee42e35c
 import os
+import random
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
@@ -7,9 +8,11 @@ from tornado.web import url
 
 
 class IndexHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.render('index.html')
-        
+    def get(self, *args, **kwargs):
+        gyudon_pics = ['gyudon.jpg']
+        image_name = random.choice(gyudon_pics)
+        self.render('index.html', image_path=self.static_url('images/' + image_name))
+
 
 class Application(tornado.web.Application):
     def __init__(self):
